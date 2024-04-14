7d76ec4c6e7c5b63f0b059bb931a3c22ea201009
Rohit <rsabi2708@gmail.com>
2024-04-14 22:37:27 +0530

    hashing pwd, salt, pepper

--- routes/notes.js
+++ routes/notes.js
@@ -1,45 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
-const { body, validationResult } = require("express-validator");
+// const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
-router.post("/", (req, res) => {
+router.post("/createUser", async (req, res) => {
   //   console.log(req.body); //use in Thunder client--> req.body --> {"name":"Rohit"}
+
+  const salt = await bcrypt.genSalt(10);
+  const secPwd = await bcrypt.hash(req.body.password, salt);
 
   let name = req.body.name;
   let email = req.body.email;
-  let pwd = req.body.password;
+  let pwd = secPwd;
   let tag = req.body.tag;
   let desc = req.body.description;
 
-  //   let sql = `INSERT INTO users (name,email,password,tag,description) VALUES(?,?,?,?,?);`;
-  //   db.query(sql, [name, email, pwd, tag, desc], (err, result) => {
-  //     if (err) {
-  //       console.log("Invalid Query", err);
-  //       res.send(err);
-  //     } else {
-  //       console.log(result);
-  //       res.send(result);
-  //     }
-  //   });
-
   let sql = `INSERT INTO users (name,email,password,tag,description) VALUES(?,?,?,?,?);`;
-  db.query(
-    sql,
-    [
-      body("name", "Enter a valid Name").isLength({ min: 3 }),
-      body("email", "Enter a valid email").isEmail(),
-      body("pwd", "Enter a valid password").isLength({ min: 5 }),
-      tag,
-      desc,
-    ],
-    (err, result) => {
-      const error = validationResult(err);
-      if (!error.isEmpty()) {
-        return res.send(`Hello, ${req.body.name}!`);
-      }
+  db.query(sql, [name, email, pwd, tag, desc], (err, result) => {
+    if (err) {
+      console.log("Invalid Query", err);
+      res.send(err);
+    } else {
+      console.log(result);
+      res.send(result);
     }
-  );
+  });
 });
+
+router.post("/createUser/:id", (req, res) => {});
 
 module.exports = router;
