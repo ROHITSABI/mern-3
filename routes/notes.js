b89ea57eba4349a9d60b3842cfcbc732349e81f0
Rohit <rsabi2708@gmail.com>
2024-04-14 21:38:21 +0530

    Validation - Please check it

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
+const { body, validationResult } = require("express-validator");
 
 router.post("/", (req, res) => {
   //   console.log(req.body); //use in Thunder client--> req.body --> {"name":"Rohit"}
@@ -11,17 +12,34 @@ router.post("/", (req, res) => {
   let tag = req.body.tag;
   let desc = req.body.description;
 
+  //   let sql = `INSERT INTO users (name,email,password,tag,description) VALUES(?,?,?,?,?);`;
+  //   db.query(sql, [name, email, pwd, tag, desc], (err, result) => {
+  //     if (err) {
+  //       console.log("Invalid Query", err);
+  //       res.send(err);
+  //     } else {
+  //       console.log(result);
+  //       res.send(result);
+  //     }
+  //   });
+
   let sql = `INSERT INTO users (name,email,password,tag,description) VALUES(?,?,?,?,?);`;
-  db.query(sql, [name, email, pwd, tag, desc], (err, result) => {
-    if (err) {
-      console.log("Invalid Query", err);
-      res.send(err);
-    } else {
-      console.log(result);
-      res.send(result);
+  db.query(
+    sql,
+    [
+      body("name", "Enter a valid Name").isLength({ min: 3 }),
+      body("email", "Enter a valid email").isEmail(),
+      body("pwd", "Enter a valid password").isLength({ min: 5 }),
+      tag,
+      desc,
+    ],
+    (err, result) => {
+      const error = validationResult(err);
+      if (!error.isEmpty()) {
+        return res.send(`Hello, ${req.body.name}!`);
+      }
     }
-  });
-  //   res.json(req.body);
+  );
 });
 
 module.exports = router;
