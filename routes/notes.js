be827daa4db1e242bc3271d9e63f7eb1a8a79f42
Rohit <rsabi2708@gmail.com>
2024-04-14 21:00:55 +0530

    POST method

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../db");
 
-router.get("/", (req, res) => {
-  console.log(req.body); //use in Thunder client--> req.body --> {"name":"Rohit"}
-  res.send("Harry");
+router.post("/", (req, res) => {
+  //   console.log(req.body); //use in Thunder client--> req.body --> {"name":"Rohit"}
+
+  let name = req.body.name;
+  let email = req.body.email;
+  let pwd = req.body.password;
+  let tag = req.body.tag;
+  let desc = req.body.description;
+
+  let sql = `INSERT INTO users (name,email,password,tag,description) VALUES(?,?,?,?,?);`;
+  db.query(sql, [name, email, pwd, tag, desc], (err, result) => {
+    if (err) {
+      console.log("Invalid Query", err);
+      res.send(err);
+    } else {
+      console.log(result);
+      res.send(result);
+    }
+  });
+  //   res.json(req.body);
 });
 
 module.exports = router;
